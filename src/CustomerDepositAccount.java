62d38d289912c4cd0e749b99f7fa34f5f2d85527
jams100 <43343134+jams100@users.noreply.github.com>
2020-03-06 03:05:06 +0000

    Moving the lodge money money functionality to the CustomerDepositAccount class from the Menu class.

--- src/CustomerDepositAccount.java
+++ src/CustomerDepositAccount.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import javax.swing.*;
+import java.util.Date;
 
 public class CustomerDepositAccount extends CustomerAccount
 {
@@ -20,5 +22,65 @@ public class CustomerDepositAccount extends CustomerAccount
 
 	public void setInterestRate(double interestRate){
 		this.interestRate = interestRate;
+	}
+
+	public void lodge(CustomerAccount acc, Customer e,JFrame f) {
+		boolean loop = true;
+		boolean on = true;
+		double balance = 0;
+		Menu m = new Menu();
+
+		if (acc instanceof CustomerCurrentAccount) {
+			int count = 3;
+			int checkPin = ((CustomerCurrentAccount) acc).getAtm().getPin();
+			loop = true;
+
+			while (loop) {
+				if (count == 0) {
+					JOptionPane.showMessageDialog(f, "Pin entered incorrectly 3 times. ATM card locked.", "Pin", JOptionPane.INFORMATION_MESSAGE);
+					((CustomerCurrentAccount) acc).getAtm().setValid(false);
+					m.customer(e);
+					loop = false;
+					on = false;
+				}
+
+				String Pin = JOptionPane.showInputDialog(f, "Enter 4 digit PIN;");
+				int i = Integer.parseInt(Pin);
+
+				if (on) {
+					if (checkPin == i) {
+						loop = false;
+						JOptionPane.showMessageDialog(f, "Pin entry successful", "Pin", JOptionPane.INFORMATION_MESSAGE);
+					} else {
+						count--;
+						JOptionPane.showMessageDialog(f, "Incorrect pin. " + count + " attempts remaining.", "Pin", JOptionPane.INFORMATION_MESSAGE);
+					}
+				}
+			}
+		}
+		if (on == true) {
+			String balanceTest = JOptionPane.showInputDialog(f, "Enter amount you wish to lodge:");
+			//This Numeric method tests to see if the string entered was numeric.
+			if (m.isNumeric(balanceTest)) {
+				balance = Double.parseDouble(balanceTest);
+				loop = false;
+
+			} else {
+				JOptionPane.showMessageDialog(f, "You must enter a numerical value!", "Oops!", JOptionPane.INFORMATION_MESSAGE);
+			}
+
+			String euro = "\u20ac";
+			acc.setBalance(acc.getBalance() + balance);
+			Date date = new Date();
+			String date2 = date.toString();
+			String type = "Lodgement";
+			double amount = balance;
+
+			AccountTransaction transaction = new AccountTransaction(date2, type, amount);
+			acc.getTransactionList().add(transaction);
+
+			JOptionPane.showMessageDialog(f, balance + euro + " added do you account!", "Lodgement", JOptionPane.INFORMATION_MESSAGE);
+			JOptionPane.showMessageDialog(f, "New balance = " + acc.getBalance() + euro, "Lodgement", JOptionPane.INFORMATION_MESSAGE);
+		}
 	}
 }
--- src/Menu.java
+++ src/Menu.java
@@ -32,6 +32,7 @@ public class Menu extends JFrame{
 	JPanel panel2;
 	JButton add;
 	String 	PPS,firstName,surname,DOB,CustomerID;
+	
 
 	public static void main(String[] args)
 	{
@@ -1224,6 +1225,7 @@ public class Menu extends JFrame{
 				}		
 			});
 
+			Customer fin = e1;
 			continueButton.addActionListener(new ActionListener(  ) {
 				public void actionPerformed(ActionEvent ae) {
 					f.dispose();
@@ -1265,6 +1267,7 @@ public class Menu extends JFrame{
 					content.add(withdrawalPanel);
 					content.add(returnPanel);
 
+					
 					statementButton.addActionListener(new ActionListener(  ) {
 						public void actionPerformed(ActionEvent ae) {
 							f.dispose();
@@ -1319,72 +1322,9 @@ public class Menu extends JFrame{
 
 					lodgementButton.addActionListener(new ActionListener(  ) {
 						public void actionPerformed(ActionEvent ae) {
-							boolean loop = true;
-							boolean on = true;
-							double balance = 0;
-
-							if(acc instanceof CustomerCurrentAccount)
-							{
-								int count = 3;
-								int checkPin = ((CustomerCurrentAccount) acc).getAtm().getPin();
-								loop = true;
-
-								while(loop)
-								{
-									if(count == 0)
-									{
-										JOptionPane.showMessageDialog(f, "Pin entered incorrectly 3 times. ATM card locked."  ,"Pin",  JOptionPane.INFORMATION_MESSAGE);
-										((CustomerCurrentAccount) acc).getAtm().setValid(false);
-										customer(e); 
-										loop = false;
-										on = false;
-									}
-
-									String Pin = JOptionPane.showInputDialog(f, "Enter 4 digit PIN;");
-									int i = Integer.parseInt(Pin);
-
-									if(on)
-									{
-										if(checkPin == i)
-										{
-											loop = false;
-											JOptionPane.showMessageDialog(f, "Pin entry successful" ,  "Pin", JOptionPane.INFORMATION_MESSAGE);
-										}
-										else
-										{
-											count --;
-											JOptionPane.showMessageDialog(f, "Incorrect pin. " + count + " attempts remaining."  ,"Pin",  JOptionPane.INFORMATION_MESSAGE);					
-										}
-									}
-								}
-							}		
-							if(on == true)
-							{
-								String balanceTest = JOptionPane.showInputDialog(f, "Enter amount you wish to lodge:");//the isNumeric method tests to see if the string entered was numeric. 
-								if(isNumeric(balanceTest))
-								{
-									balance = Double.parseDouble(balanceTest);
-									loop = false;
-								}
-								else
-								{
-									JOptionPane.showMessageDialog(f, "You must enter a numerical value!" ,"Oops!",  JOptionPane.INFORMATION_MESSAGE);
-								}
-
-								String euro = "\u20ac";
-								acc.setBalance(acc.getBalance() + balance);
-								// String date = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
-								Date date = new Date();
-								String date2 = date.toString();
-								String type = "Lodgement";
-								double amount = balance;
-
-								AccountTransaction transaction = new AccountTransaction(date2, type, amount);
-								acc.getTransactionList().add(transaction);
-
-								JOptionPane.showMessageDialog(f, balance + euro + " added do you account!" ,"Lodgement",  JOptionPane.INFORMATION_MESSAGE);
-								JOptionPane.showMessageDialog(f, "New balance = " + acc.getBalance() + euro ,"Lodgement",  JOptionPane.INFORMATION_MESSAGE);
-							}
+							 CustomerDepositAccount depositAccount = new CustomerDepositAccount();
+	                            depositAccount.lodge(acc, fin, f);
+	                            
 						}	
 					});
 
